1df12decd074d0f837cd990a893456314fc63121
simonhir <14137977+simonhir@users.noreply.github.com>
2026-08-13 14:04:17 +0200

    chore(v2/gateway): rm AttributUriUndWert mapper

--- v2/eakte-api-v2-gateway/src/main/java/de/muenchen/oss/eakte/api/v2/gateway/adapter/in/rest/AttributeMapper.java
+++ v2/eakte-api-v2-gateway/src/main/java/de/muenchen/oss/eakte/api/v2/gateway/adapter/in/rest/AttributeMapper.java
@@ -1,140 +1,27 @@
 package de.muenchen.oss.eakte.api.v2.gateway.adapter.in.rest;
 
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributAggregatstyp;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributAufzaehlungstyp;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributBase64EncodedContent;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributBoolescherWert;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributDatenTyp;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributDatum;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributDatumMitUhrzeit;
 import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributEintrag;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributFliesskommazahl;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributGanzzahl;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributUriUndWert;
-import de.muenchen.itm.eakte.schnittstelle.rest_v2.server_stubs.model.AttributZeichenkette;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.AggregateAttribute;
 import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.Attribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.AttributeType;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.BooleanAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.ContentAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.DateAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.DatetimeAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.EnumAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.FloatAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.IntegerAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.ObjectAttribute;
-import de.muenchen.oss.eakte.api.v2.gateway.domain.model.attribute.StringAttribute;
-import java.util.Base64;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
-public abstract class AttributeMapper {
-    private final Base64.Encoder b64Encoder = Base64.getEncoder();
-
-    Map<String, Object> mapAttributMap(final List<Attribute<?>> attributes) {
+abstract class AttributeMapper {
+    protected Map<String, Object> mapAttributMap(final List<Attribute<?>> attributes) {
         return attributes.stream().collect(Collectors.toMap(
                 i -> String.format("%s_%s", i.getReference(), i.getIndex()),
                 Attribute::getValue));
     }
 
-    abstract List<AttributEintrag> mapAttributEintragListe(List<Attribute<?>> attributes);
+    protected abstract List<AttributEintrag> mapAttributEintragListe(List<Attribute<?>> attributes);
 
-    AttributEintrag mapAttributEintrag(final Attribute<?> attribute) {
+    protected AttributEintrag mapAttributEintrag(final Attribute<?> attribute) {
         return new AttributEintrag(attribute.getReference(), attribute.getValue());
-    }
-
-    abstract List<AttributUriUndWert> mapAttributUriUndWertListe(List<Attribute<?>> attributes);
-
-    AttributUriUndWert mapAttributUriUndWert(final Attribute<?> attribute) {
-        return switch (attribute) {
-        case StringAttribute i -> mapString(i);
-        case BooleanAttribute i -> mapBoolean(i);
-        case FloatAttribute i -> mapFloat(i);
-        case IntegerAttribute i -> mapInt(i);
-        case DateAttribute i -> mapDate(i);
-        case DatetimeAttribute i -> mapDatetime(i);
-        case EnumAttribute i -> mapEnum(i);
-        case ContentAttribute i -> mapContent(i);
-        case ObjectAttribute i -> mapObject(i);
-        case AggregateAttribute i -> mapAggregate(i);
-        default -> throw new IllegalStateException("No response mapper for %s implemented".formatted(attribute.getClass()));
-        };
-    }
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributZeichenkette mapString(StringAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributBoolescherWert mapBoolean(BooleanAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributFliesskommazahl mapFloat(FloatAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributGanzzahl mapInt(IntegerAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributDatum mapDate(DateAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributDatumMitUhrzeit mapDatetime(DatetimeAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributAufzaehlungstyp mapEnum(EnumAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributBase64EncodedContent mapContent(ContentAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributZeichenkette mapObject(ObjectAttribute attribute);
-
-    @Mapping(source = "value", target = "attributWert")
-    @Mapping(source = "reference", target = "attributURI")
-    @Mapping(source = "fabasoftType", target = "datenTyp")
-    abstract AttributAggregatstyp mapAggregate(AggregateAttribute attribute);
-
-    String mapB64(byte[] data) {
-        return b64Encoder.encodeToString(data);
-    }
-
-    String mapType(AttributeType type) {
-        return switch (type) {
-        case STRING -> AttributDatenTyp.ZEICHENKETTE.getValue();
-        case BOOLEAN -> AttributDatenTyp.BOOLESCHER_WERT.getValue();
-        case INTEGER -> AttributDatenTyp.GANZZAHL.getValue();
-        case FLOAT -> AttributDatenTyp.FLIESSKOMMAZAHL.getValue();
-        case DATE -> AttributDatenTyp.DATUM.getValue();
-        case DATETIME -> AttributDatenTyp.DATUM_MIT_UHRZEIT.getValue();
-        case ENUM -> AttributDatenTyp.AUFZAEHLUNGSTYP.getValue();
-        case CONTENT -> AttributDatenTyp.BASE64_ENCODED_CONTENT.getValue();
-        // TODO correctly map object
-        case OBJECT -> AttributDatenTyp.ZEICHENKETTE.getValue();
-        case AGGREGATE -> AttributDatenTyp.AGGREGATSTYP.getValue();
-        };
     }
 
     /**
@@ -147,7 +34,7 @@ public abstract class AttributeMapper {
      * @param attributes The list of attributes to map.
      * @return The mapped attributes.
      */
-    public Map<String, Object> toUniqueReferenceValueMap(
+    protected Map<String, Object> toUniqueReferenceValueMap(
             final List<Attribute<?>> attributes) {
         return attributes.stream()
                 // map to multi value map
@@ -167,7 +54,7 @@ public abstract class AttributeMapper {
      * @return The value of the key.
      * @throws IllegalArgumentException If the type doesn't match the value.
      */
-    public <T> Optional<T> getTyped(
+    protected <T> Optional<T> getTyped(
             final Map<String, Object> valueMap,
             final String key,
             final Class<T> type) {
