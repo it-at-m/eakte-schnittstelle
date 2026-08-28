1ebe1472779cb9e847d9ac88118fa75632e2ef77
Frank Hofmann <frank.hofmann1@muenchen.de>
2026-08-28 14:26:05 +0200

    remove poc (#37)

--- v2/eakte-api-v2-spring-boot-api-gateway-poc/mvn-spring-boot-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-cd "${SCRIPT_DIR}" || exit 1
-tput rmam
-mvn spring-boot:run -Dspring-boot.run.arguments="--spring.config.additional-location=classpath:application-test-credentials.yaml"
